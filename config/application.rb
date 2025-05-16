156c6fc77a5a9819b859339dbcee2c74d9e58f09
linoh <linoh@linkey1125.>
2025-05-17 00:09:29 +0900

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('test/mailers/previews')
+
   end
 end
