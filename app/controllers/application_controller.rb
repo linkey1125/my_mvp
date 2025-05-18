7a0c1d37e282a1105df35ca0018f3784b91efe76
linoh <linoh@linkey1125.>
2025-05-19 00:35:39 +0900

    ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  helper_method :current_user, :logged_in?
   allow_browser versions: :modern
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authenticate_user!
+    redirect_to login_path, alert: "ログインが必要です" unless logged_in?
+  end
 end
