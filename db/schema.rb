8934c7151c1560e76229ccb2148235204f42818f
linoh <linoh@linkey1125.>
2025-07-13 18:27:34 +0900

    ランキング機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_13_083444) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_13_085634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_13_083444) do
     t.string "category", null: false
     t.string "price_range", null: false
     t.string "uv_cut_rate", null: false
+    t.integer "view_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
