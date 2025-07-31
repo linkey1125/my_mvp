2c810339ec57f3e43ac90700f9dee90728b1ccde
linoh <linoh@linkey1125.>
2025-07-31 22:54:25 +0900

    エラー修正

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
+# config/environment.rb
+
 # Load the Rails application.
-require_relative "application"
+require_relative "boot"                # ← まず boot.rb を読み込む
+require "rails/all"
+require "dotenv/load"
+
+# Require the gems listed in Gemfile
+Bundler.require(*Rails.groups)
+
+require_relative "application"        # ← application.rb を読み込む
 
 # Initialize the Rails application.
 Rails.application.initialize!
