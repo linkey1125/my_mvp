597604f1c853b5a43476157ee72f3836710371c4
linoh <linoh@linkey1125.>
2025-07-31 22:02:15 +0900

    画像修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
 
   # Enable server timing.
   config.server_timing = true
+  config.active_storage.service = :amazon
+
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
