82f0d66164a759e1a8b77fa3572320eceea591a9
linoh <linoh@linkey1125.>
2025-08-03 22:55:32 +0900

    ドメイン修正

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,4 +1,6 @@
 class PasswordMailer < ApplicationMailer
+  default from: "no-reply@uvtechtechto.com"  # ← 認証済みドメインのアドレスを指定
+
   def password_reset
     @user = params[:user]
     mail(to: @user.email, subject: "パスワードリセットのご案内")
