3773d172c1a8e7b9ecf1537969db0a0d7726ebe1
linoh <linoh@linkey1125.>
2025-08-03 22:25:11 +0900

    ドメイン修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,12 +23,12 @@ Rails.application.configure do
 # メール設定（SendGrid用）
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_caching = false
-config.action_mailer.default_url_options = { host: "your-app.onrender.com", protocol: "https" }
+config.action_mailer.default_url_options = { host: "uvtechtechto.com", protocol: "https" }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: "smtp.sendgrid.net",
   port: 587,
-  domain: "your-app.onrender.com",
+  domain: "uvtechtechto.com"
   user_name: "apikey", # ← 固定
   password: ENV["SENDGRID_API_KEY"], # ← Renderの環境変数に設定したAPIキー
   authentication: "plain",
